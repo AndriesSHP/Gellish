ec1dcf6ad21c080544a701dcb61fe3fac57e2a08
Andries van Renssen <andries.vanrenssen@gellish.net>
2016-11-05 14:39:14 +0100

    Formal Language and Bootstrapping
    Choose the formal language (currently either English or Nederlands (Dutch)) and initialize the bootstrapping kinds of relations.

---
+++ BootstrappingRelations.py
@@ -0,0 +1,15 @@
+baseBootPhrasesEN    = ["is a kind of",\
+                        "has by definition as first role a",\
+                        "has by definition as second role a",\
+                        "is a base phrase for",\
+                        "is an inverse phrase for",\
+                        "is a synonym of"]
+inverseBootPhrasesEN = ["is by definition a role of a"]
+baseBootPhrasesNL    = ["is een soort",\
+                        "heeft per definitie als eerste rol een",\
+                        "heeft per definitie als tweede rol een",\
+                        "is een basis frase voor",\
+                        "is een inverse frase voor",\
+                        "is synoniem van"]
+inverseBootPhrasesNL = ["is per definitie een rol van een"]
+base_relation_types  = [1146, 5944, 5945, 6066, 1986, 1981, 5343]
